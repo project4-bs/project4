16b6fe4e2cf11c8ddc1b586d5e6ca66ce3d3044a
twigim2 <twigim2@naver.com>
2024-03-18 14:12:40 +0900

    20240318 14:12

--- infra/vpc/output.tf
+++ infra/vpc/output.tf
@@ -2,18 +2,18 @@ output "vpc_id" {
   value = aws_vpc.project04-vpc.id
 }
 
-output "public-subnet-2a-id" {
-    value = aws_subnet.project04-public-subnet-2a.id
+output "public1-subnet-2a-id" {
+    value = aws_subnet.project04-subnet-public1-2a.id
 }
 
-output "public-subnet-2c-id" {
-    value = aws_subnet.project04-public-subnet-2c.id
+output "public2-subnet-2c-id" {
+    value = aws_subnet.project04-subnet-public2-2c.id
 }
 
-output "private-subnet-2a-id" {
-    value = aws_subnet.project04-private-subnet-2a.id
+output "private1-subnet-2a-id" {
+    value = aws_subnet.project04-subnet-private1-2a.id
 }
 
-output "private-subnet-2c-id" {
-    value = aws_subnet.project04-private-subnet-2c.id
+output "private2-subnet-2c-id" {
+    value = aws_subnet.project04-subnet-private2-2c.id
 }
