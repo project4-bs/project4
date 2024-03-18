17d58297a398af504c5e5345eb91847aeef71f18
Lucas0828 <augdh1@naver.com>
2024-03-18 11:57:47 +0900

    오타수정

--- globals/s3/backend.tf
+++ globals/s3/backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3"{
     bucket  =  "project04-s3-bucket"
-    regin   =  "ap-northeast-2"
+    region  =  "ap-northeast-2"
     key     =  "globals/s3/terrafrom.tfstate"
     dynamodb_table = "project04-terraform-locks"
     encrypt =  true
