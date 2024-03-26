129836ae46bee5514fd88212a14730a314296f6a
Lucas0828 <augdh1@naver.com>
2024-03-26 10:40:45 +0900

    2024.03.26. 오타수정

--- infra/ec2/sg/remote.tf
+++ infra/ec2/sg/remote.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
-    backend = "s3"
-    config = {
-        bucket = "project04-s3-bucket"
-        key = "infra/vpc/terraform.tfstate"
-        region = "ap-northeast-2"
-    }
+  backend = "s3"
+  config = {
+    bucket = "project04-s3-bucket"
+    key    =  "infra/vpc/terrafrom.tfstate"
+    region = "ap-northeast-2"
+  }
 }
