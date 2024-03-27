fd542e5d9d00f925f8e625bebd9bda6d6bc7caed
Lucas0828 <augdh1@naver.com>
2024-03-27 09:57:29 +0900

    2024.03.27. jenkins 미완

---
+++ infra/ec2/jenkins/templates/userdata.sh
@@ -0,0 +1,14 @@
+# #!/bin/bash
+
+# sudo apt update -y
+# sudo apt install -y ruby wget openjdk-17-jdk docker.io git curl zip unzip
+
+# cd /home/ubuntu
+# git clone https://github.com/twigim2/aws-project.git
+# sudo chown -R ubuntu:ubuntu aws-project
+
+# cd /home/ubuntu/aws-project
+# chmod u+x install-docker.sh && sudo ./install-docker.sh
+# chmod u+x install-docker-compose.sh && sudo ./install-docker-compose.sh
+
+# sudo docker-compose up -d --build
